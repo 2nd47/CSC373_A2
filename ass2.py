11e2e1255f85ae0d43185fba5bfbaece28c74462
2nd47 <2nd4777@gmail.com>
2016-10-20 17:04:01 -0400

    __getitem__ on Node
    - can now index a node and be returned its children value at the
    specified index

--- ass2.py
+++ ass2.py
@@ -5,12 +5,15 @@ class Node:
 		#A list of tuples containing edge weight and child node
 		self.children = []
 
+	def __getitem__(self, key):
+		# Access a node with [] notation and you will access its children
+		return self.children[key]
+
 def create_graph(size):
-	
+
 
 
 if __name__ == '__main__':
 	sizev = sys.argv[1]
 
 	create_graph(sizev)
-
