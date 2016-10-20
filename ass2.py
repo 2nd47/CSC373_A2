dea4b983b1f166c9ec146c1b4d8ae85c0f8a7697
jpaprakis <j.paprakis@mail.utoronto.ca>
2016-10-20 16:42:34 -0400

    lala

--- ass2.py
+++ ass2.py
@@ -1,1 +1,16 @@
-# stub file
+import sys
+
+class Node:
+	def __init__(self):
+		#A list of tuples containing edge weight and child node
+		self.children = []
+
+def create_graph(size):
+	
+
+
+if __name__ == '__main__':
+	sizev = sys.argv[1]
+
+	create_graph(sizev)
+
