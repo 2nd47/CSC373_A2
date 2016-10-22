49aaee6154a948a7183046da068f43d6bf03ea2c
2nd47 <2nd4777@gmail.com>
2016-10-21 21:02:44 -0400

    Major update, create nodes for parts 1 and 2
    Node:
    - init now takes a size argument and an optional position parameter for
    part 2
    - string and repr representation for printing the list
    
    - dist function to calculate Euclidean distance between two points
    
    - randomWithinCircle creates a random position within the unit circle
    
    - create_graph now takes a edgeFunc or posFunc which then determines
    which question will be run
    
    - currently, we print the nodelist to the console for quick debugging
    for major issues

--- ass2.py
+++ ass2.py
@@ -1,41 +1,86 @@
+import math
 import sys
 from random import random
 import pdb
 
 class Node:
-	def __init__(self, val, size):
-		self.value = val
-		#A list of weights to each node w/ idx corresponding to val
+	def __init__(self, size, pos=(0,0)):
+		self.pos = pos
+		# a list of weights to each node w/ idx corresponding to weight
 		self.children = [None]*size
 
 	def __getitem__(self, key):
-		# Access a node with [] notation and you will access its children
+		'''# access a node with [] notation and you will access its children
+		'''
 		return self.children[key]
 
 	def __setitem__(self, key, value):
-		# Access a node with [] notation and you will access its children
+		'''access a node with [] notation and you will access its children
+		'''
 		self.children[key] = value
 
-def create_graph(size):
-	nodelist = []
-	for i in range(size):
-		nodelist.append(Node(i, size))
+	def __str__(self):
+		'''string representation of some node
+		'''
+		return 'Node with ' + str(self.children) + '\n'
 
-	#set edges
+	def __repr__(self):
+		return self.__str__()
+
+'''
+REMINDER : THESE FILES MUST BE SPLIT UP BEFORE SUBMISSION
+'''
+
+def dist(pos1, pos2):
+	'''return the distance between two points which are represented as
+	a two-element, one-dimensional array
+	'''
+	return math.sqrt(
+		(pos1[0] - pos2[0])**2 +
+		(pos1[1] - pos2[1])**2
+	)
+
+def randomWithinCircle():
+	''' Create a random position within the unit circle
+	'''
+	# create a random point along the circumfrence of the circle
+	z = 2*math.pi*random()
+	return (math.cos(z), math.sin(z))
+
+def create_graph(size, edgeFunc, posFunc):
+	# create an empty array of length 'size' with empty nodes
+	nodelist = []
+	if posFunc:
+		nodelist = [Node(size, posFunc()) for i in range(size)]
+	else:
+		nodelist = [Node(size) for i in range(size)]
+
+	# for every node in the nodelist
 	for i in range(size):
-		for j  in range(size):
-			if not nodelist[i][j]:
+		# and every one of its children (don't need all nodes)
+		for j in range(i, size):
+			# if not already initialized
+			if i <= j and (not nodelist[i][j]):
+				# if the edge is to itself, the weight must be 0
 				if i == j:
 					nodelist[i][j] = 0
+				# otherwise, initialize some random weight for the edge
 				else:
-					edgelen = random()
+					# initialize the edge weight given some positions
+					if posFunc:
+						edgelen = dist(nodelist[i].pos, nodelist[j].pos)
+					# initialize the edge weight given some function
+					else:
+						edgelen = edgeFunc()
 					nodelist[i][j] = edgelen
 					nodelist[j][i] = edgelen
-
+	print(nodelist)
 	pdb.set_trace()
 
 if __name__ == '__main__':
-	sizev = int(sys.argv[1])
-
-	#size can be between 30 - 50k
-	create_graph(sizev)
+	# size can be between 30 - 50k
+	if len(sys.argv) < 2:
+		print('Please input a size argument!')
+		exit()
+	create_graph(int(sys.argv[1]), random, None)
+	#create_graph(int(sys.argv[1]), None, randomWithinCircle)
