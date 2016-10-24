98d206cf5227dbc37328e0e2c5552f17100a61df
2nd47 <2nd4777@gmail.com>
2016-10-24 18:26:18 -0400

    java!!!

---
+++ Java/GraphVertex.java
@@ -0,0 +1,12 @@
+/**
+ * Created by 2nd47 on 2016-10-24.
+ */
+public class GraphVertex {
+        public double cost = 1.1d;
+        public int index;
+        public GraphVertex prev;
+
+        public GraphVertex(int index) {
+            this.index = index;
+        }
+}
