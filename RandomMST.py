5bcd5fa0036b31601636b5cadfdd1fc12cdc9aca
Cynthia (Qingwei) Li <cynngah@gmail.com>
2016-10-24 14:50:09 -0400

    remove timer

--- RandomMST.py
+++ RandomMST.py
@@ -22,33 +22,23 @@ def randomMST(size):
 	current = 0
 	h = []
 	weight = 0
-	heaptime = 0
-	poptime = 0
 
 	while (len(edges) < size - 1):
-		start = time.time()
 		for i in not_in_mst:
 			if (not nodes[i] and i != current):
 				heapq.heappush(h, Edge(current, i, random()))
-		end = time.time()
 
-		heaptime += end - start
 		min_edge = heapq.heappop(h)
 
-		start = time.time()
 		while (nodes[min_edge.vertices[0]] and nodes[min_edge.vertices[1]]):
 			min_edge = heapq.heappop(h)
-		end = time.time()
 
-		poptime += end - start
 		current = min_edge.vertices[1]
 		nodes[current] = 1
 		edges.append(min_edge)
 		weight += min_edge.weight
 		not_in_mst.remove(current)
 
-	print("heaptime " + str(heaptime))
-	print("poptime " + str(poptime))
 	return weight
 
 if __name__ == '__main__':
@@ -57,8 +47,5 @@ if __name__ == '__main__':
 		print('Please input a size argument!')
 		exit()
 
-	start = time.time()
 	print(randomMST(int(sys.argv[1])))
-	end = time.time()
-	print(end - start)
 	#create_graph(int(sys.argv[1]), None, randomWithinCircle)
